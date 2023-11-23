995e6b2ffae7397db0a13f6cdd086c43d8f616c4
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-23 05:07:58 +0000

    init desde 0

--- proyecto/trabajadores/views.py
+++
@@ -1,9 +0,0 @@
-from .models import Usuario
-from rest_framework import viewsets
-from rest_framework import permissions
-from proyecto.trabajadores.serializers import UsuarioSerializer
-
-class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all().order_by('-date_joined')
-    serializer_class = UsuarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
