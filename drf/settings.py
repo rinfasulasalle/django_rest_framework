ca070427d2c2a13a5f889e60483adad8d3b9ee58
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-27 21:22:08 +0000

    agrego CORSS

--- drf/settings.py
+++ drf/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'coreapi',
     'api',
@@ -50,7 +51,39 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+# -----------------------
+CORS_ALLOWED_ORIGINS = [
+    "http://62.72.11.15:3000",
+    "http://62.72.11.15:4000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# -----------------------
+
 
 ROOT_URLCONF = 'drf.urls'
 
