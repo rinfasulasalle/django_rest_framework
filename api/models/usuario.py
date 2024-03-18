2a5179000113209fe1be2a91c80fb422e14b7bcf
rinfasulasalle <rinfas@ulasalle.edu.pe>
2024-03-18 01:41:15 -0500

    Usuario - Trabajador funcionan

--- api/models/usuario.py
+++ api/models/usuario.py
@@ -7,12 +7,11 @@ class Usuario(models.Model):
     id = models.CharField(max_length=20, primary_key=True, unique=True)
     id_usuario_rol = models.ForeignKey(DropdownRoles, on_delete=models.CASCADE)
     usuario_nombres = models.CharField(max_length=100)
-    usuario_apellidos = models.CharField(max_length=100)
+    usuario_apellido_paterno = models.CharField(max_length=100)
+    usuario_apellido_materno = models.CharField(max_length=100)
     usuario_correo = models.EmailField(unique=True)
     usuario_contrasenia = models.CharField(max_length=100)
-    usuario_sexo = models.CharField(max_length=15, choices=[('Masculino', 'Masculino'),('Femenino', 'Femenino'),('NE', 'No Especificado')], default='NE')
-    usuario_telefono = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.id}, {self.usuario_nombres} {self.usuario_apellidos}"
+        return f"{self.id}: {self.usuario_nombres} {self.usuario_apellido_paterno}, {self.usuario_apellido_materno}"
